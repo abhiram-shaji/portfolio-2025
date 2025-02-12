0bee6d62a48ea39364a21e14aa5e0513fe0ee062
Abhiram <write4abhiram@gmail.com>
2025-02-12 00:44:56 -0800

    Update seo

--- src/data/seo.js
+++ src/data/seo.js
@@ -2,36 +2,36 @@ const SEO = [
 	{
 		page: "home",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"I am a full-stack developer with expertise in React, Node.js, and .NET. I have experience in building scalable, maintainable web and mobile applications with a strong focus on performance and user experience.",
+		keywords: ["Abhiram", "Abhiram Shaji", "Abhiram Kace"],
 	},
 
 	{
 		page: "about",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"I am a full-stack developer with expertise in React, Node.js, and .NET. I have experience in building scalable, maintainable web and mobile applications with a strong focus on performance and user experience.",
+		keywords: ["Abhiram", "Abhiram Shaji", "Abhiram Kace"],
 	},
 
 	{
 		page: "articles",
 		description:
-			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"Chronological collection of my long-form thoughts on software development, full-stack engineering, agile methodologies, and emerging technologies.",
+		keywords: ["Abhiram", "Abhiram Shaji", "Abhiram Kace"],
 	},
 
 	{
 		page: "projects",
 		description:
-			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"I've worked on a variety of projects over the years, focusing on real-world applications that optimize workflows and improve user experiences. Many of these projects are open-source and available for exploration and contribution.",
+		keywords: ["Abhiram", "Abhiram Shaji", "Abhiram Kace"],
 	},
 
 	{
 		page: "contact",
 		description:
-			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"If you're interested in collaborating on a project or discussing potential opportunities, feel free to reach out. I'm always open to new ideas and challenges.",
+		keywords: ["Abhiram", "Abhiram Shaji", "Abhiram Kace"],
 	},
 ];
 
