9d8849bc723fe38f66a5a0c41f2b711f769c5df5
Abhiram <write4abhiram@gmail.com>
2025-02-12 01:56:32 -0800

    Update project card layout and data

--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -6,30 +6,33 @@ import { faLink } from "@fortawesome/free-solid-svg-icons";
 import "./styles/project.css";
 
 const Project = (props) => {
-	const { logo, title, description, linkText, link } = props;
+  const { logo, title, description, linkText, link } = props;
 
-	return (
-		<React.Fragment>
-			<div className="project">
-				<Link to={link}>
-					<div className="project-container">
-						<div className="project-logo">
-							<img src={logo} alt="logo" />
-						</div>
-						<div className="project-title">{title}</div>
-						<div className="project-description">{description}</div>
-						<div className="project-link">
-							<div className="project-link-icon">
-								<FontAwesomeIcon icon={faLink} />
-							</div>
-
-							<div className="project-link-text">{linkText}</div>
-						</div>
-					</div>
-				</Link>
-			</div>
-		</React.Fragment>
-	);
+  return (
+    <div className="project">
+      <Link to={link}>
+        {/* 
+          Use the logo as a background image. 
+          Inline style is used here for demonstration so that it's dynamic, 
+          but you could also pass a className that sets the background in CSS. 
+        */}
+        <div
+          className="project-container"
+          style={{ backgroundImage: `url(${logo})` }}
+        >
+          {/* Overlay to dim the background and make text readable */}
+          <div className="overlay">
+            <h3 className="project-title">{title}</h3>
+            <p className="project-description">{description}</p>
+            <div className="project-link">
+              <FontAwesomeIcon icon={faLink} className="project-link-icon" />
+              <span className="project-link-text">{linkText}</span>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Project;
--- src/data/user.js
+++ src/data/user.js
@@ -48,15 +48,15 @@ const INFO = {
 		{
 			title: "Langroove",
 			description: "A social language learning platform with real-time messaging and translation features.",
-			logo: "https://worksofabhiram.com/img/langroove.png",
+			logo: "https://i.ibb.co/CB4kLsF/splash.png",
 			linkText: "GitHub",
 			link: "https://github.com/abhiram-shaji/Langroove",
 		},
 	
 		{
-			title: "Comox Valley Writers Society Conference Website",
+			title: "Conference Website",
 			description: "A website for the North Island Writers Conference, featuring event registration and presenter details.",
-			logo: "https://worksofabhiram.com/img/cvws.png",
+			logo: "https://cvwriterssociety.ca/wp-content/uploads/2017/12/cropped-comox-commercial-docs.jpg",
 			linkText: "Preview",
 			link: "https://cvwsconference.web.app/",
 			github: "https://github.com/abhiram-shaji/Conference-Website",
@@ -65,7 +65,7 @@ const INFO = {
 		{
 			title: "Asteroid Tracker",
 			description: "A web app using Leaflet.js and NASA's NeoWs API to track Near-Earth Objects.",
-			logo: "https://worksofabhiram.com/img/NEOT.png",
+			logo: "https://images.moneycontrol.com/static-mcnews/2024/07/20240711103206_Sushi-46.jpg?impolicy=website&width=1600&height=900",
 			linkText: "Preview",
 			link: "https://abhiram-shaji.github.io/Asteroid-Tracker/",
 			github: "https://github.com/abhiram-shaji/Asteroid-Tracker",
