78148f26309dcf5c79dc84a237eb9565a749dee6
Abhiram <write4abhiram@gmail.com>
2025-02-14 00:19:03 -0800

    Update project css

--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -10,27 +10,20 @@ const Project = (props) => {
 
   return (
     <div className="project">
-      <Link to={link}>
-        {/* 
-          Use the logo as a background image. 
-          Inline style is used here for demonstration so that it's dynamic, 
-          but you could also pass a className that sets the background in CSS. 
-        */}
-        <div
-          className="project-container"
-          style={{ backgroundImage: `url(${logo})` }}
-        >
-          {/* Overlay to dim the background and make text readable */}
-          <div className="overlay">
-            <h3 className="project-title">{title}</h3>
-            <p className="project-description">{description}</p>
-            <div className="project-link">
-              <FontAwesomeIcon icon={faLink} className="project-link-icon" />
-              <span className="project-link-text">{linkText}</span>
-            </div>
-          </div>
+      <div
+        className="project-container"
+        style={{ backgroundImage: `url(${logo})` }}
+      >
+        <div className="overlay">
+          <h3 className="project-title">{title}</h3>
+          <p className="project-description">{description}</p>
+          {/* Wrap only the project link in <Link> */}
+          <Link to={link} className="project-link">
+            <FontAwesomeIcon icon={faLink} className="project-link-icon" />
+            <span className="project-link-text">{linkText}</span>
+          </Link>
         </div>
-      </Link>
+      </div>
     </div>
   );
 };
